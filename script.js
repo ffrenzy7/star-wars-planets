fdbfdfd7e4aef096edf7825262f0b8e96a2fe363
Renan Alonso <renan.alonso91@gmail.com>
2024-04-27 06:45:51 -0300

    fix: removed parallax on drag for mobile

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const formResult = document.querySelector('.formResult')
 const closeResultButton = document.querySelector('.closeResultButton')
 
 container.addEventListener('mousemove', parallax)
-container.addEventListener('touchmove', parallax)
+// container.addEventListener('touchmove', parallax)
 modalButton.addEventListener('click', openModal)
 modalBackground.addEventListener('click', closeModal)
 modalCloseButton.addEventListener('click', closeModal)
@@ -41,8 +41,10 @@ nextButton.addEventListener('touchend', backToNormal)
 function parallax(event) {
   const width = window.innerWidth / 2
   const height = window.innerHeight / 2
-  const mouseX = event.clientX || event.touches[0].clientX
-  const mouseY = event.clientY || event.touches[0].clientY
+  const mouseX = event.clientX
+  //  || event.touches[0].clientX
+  const mouseY = event.clientY
+  //  || event.touches[0].clientY
   const depth = `${50 - (mouseX - width) * 0.01}% ${
     50 - (mouseY - height) * 0.01
   }%`
